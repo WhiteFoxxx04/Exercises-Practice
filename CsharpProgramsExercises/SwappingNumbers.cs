8e06aa09c7a834730cc39473bb69f42e4f275e8a
Whitefoxxx02 <asgarkhan16@gmail.com>
2021-01-04 19:01:39 +0530

    Add project files.

---
+++ CsharpProgramsExercises/SwappingNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpProgramsExercises
+{
+    class SwappingNumbers
+    {
+        public static void main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ SwappingNumbers/SwappingNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwappingNumbers
+{
+    //how to swap two numbers without using a temp variable in C#.
+    class SwappingNumbers
+    {
+        static void Main(string[] args)
+        {
+            //declaration
+            int first, second;
+            
+            //initializing
+            first = 100;
+            second = 200;
+
+            //logic
+            first = first + second;
+            second = first - second;
+            first = first - second;
+
+            Console.WriteLine(first.ToString());
+            Console.WriteLine(second.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
