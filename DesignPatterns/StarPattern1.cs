9073f1a84331dd8a6badce81c7923019fc864af1
Whitefoxxx02 <asgarkhan16@gmail.com>
2021-01-05 21:40:14 +0530

    Patterns

---
+++ DesignPatterns/StarPattern1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class StarPattern1
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Enter the number for pattern");
+            c = int.Parse(Console.ReadLine());
+
+            for (a = 0; a <= c; a++)
+            {
+                for (b = 1; b <= a; b++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
+    
