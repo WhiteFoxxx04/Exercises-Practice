8d0c466c90fb38f83e7b9cdbb78d88b1d8bed176
Whitefoxxx02 <asgarkhan16@gmail.com>
2021-01-06 20:55:42 +0530

    Prime Numbers done

--- PrimeNumbers/Program.cs
+++ PrimeNumbers/Program.cs
@@ -10,6 +10,23 @@ namespace PrimeNumbers
     {
         static void Main(string[] args)
         {
+            int n, i, m = 0, flag = 0;
+            Console.Write("Enter the number to find out if its a Prime Number or Not: ");
+            n = int.Parse(Console.ReadLine());
+            m = n / 2;
+            for (i = 2; i <= m; i++)
+            {
+                if(n % i == 0)
+                {
+                    Console.Write("Not a Prime Number");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+                Console.Write("Prime Number");
+
+            Console.Read();
         }
     }
 }
