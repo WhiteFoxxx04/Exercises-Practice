519336f6f51919d31e0386c6ef086c1f1e05e8d4
Whitefoxxx02 <asgarkhan16@gmail.com>
2021-01-08 04:01:59 +0530

    Factorial done

---
+++ FactorialNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to get a factorial: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int fact = 1;
+
+            for (int i = 1; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial of the given number is : " + fact);
+        }
+    }
+}
